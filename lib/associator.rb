f7cd126966f9a0b947de3dd8997375f92bb834e7
mabarroso <mabarroso@mabarroso.com>
2012-02-16 21:00:50 +0100

    Fix finder_sql

--- lib/associator.rb
+++ lib/associator.rb
@@ -23,14 +23,23 @@ class ActiveRecord::Base
 #			has_many @to_type, :through => "Association_#{@from_type}_#{@to_type}", :foreign_key => 'to', :conditions => {:from_type => @from_type, :to_type => @to_type}
 #			has_and_belongs_to_many @to_type, :join_table => :associations, :foreign_key => 'from', :association_foreign_key => 'to', :conditions => {"association.from_type" => @from_type, "association.to_type" => @to_type}
 
-			has_and_belongs_to_many :ar, :join_table => :associations, :finder_sql => "
+      has_many @to_type, :finder_sql => "
 				SELECT 'groups'.* FROM 'groups'
-					INNER JOIN 'associations' ON ('groups'.'id' = 'associations'.'to' AND 'associations'.'from' = 1 AND 'association'.'from_type' = 'group' AND 'association'.'to_type' = 'group')
+					INNER JOIN 'associations' ON ('groups'.'id' = 'associations'.'from'
+					  AND 'associations'.'to' = 4
+					  AND 'associations'.'from_type' = 'group'
+					  AND 'associations'.'to_type' = 'group'
+					)
 			"
+#			has_and_belongs_to_many :ar, :join_table => :associations, :finder_sql => "
+#				SELECT 'groups'.* FROM 'groups'
+#					INNER JOIN 'associations' ON ('groups'.'id' = 'associations'.'to' AND 'associations'.'from' = 1 AND 'association'.'from_type' = 'group' AND 'association'.'to_type' = 'group')
+#			"
 		end
 	end
 
   def add_associated obj
+    @from_type = obj.class.name.downcase
   	Associator::Association.create(:from => id, :from_type => @from_type, :to => obj.id, :to_type => obj.name.downcase, :key => "#{id}_#{obj.id}")
 	end
 
