c004435abd767cbf6c84d18edc0d1a2aad10a4b4
mabarroso <mabarroso@mabarroso.com>
2012-02-16 23:30:43 +0100

    Engines mountables support

--- lib/associator.rb
+++ lib/associator.rb
@@ -8,23 +8,39 @@ class ActiveRecord::Base
 		@@from_type = name.downcase
 		if opts[:with]
 			@@to_type = opts[:with].to_s.downcase
+			@@class_name = opts[:class_name] || false
+			@@table_name = @@to_type.pluralize if @@to_type
+			@@table_name = @@class_name.pluralize if @@class_name
+			@@table_name = opts[:table] if opts[:table]
+
 			# http://stackoverflow.com/questions/8545360/finder-sql-does-not-parse-string-with-rails
-      has_many @@to_type, :finder_sql => ->(record) do
-        record = self if(record.nil?)
-        "
-				SELECT 'groups'.* FROM groups
-					INNER JOIN 'associations' ON ('groups'.'id' = 'associations'.'to'
-					  AND 'associations'.'from' = #{record.id}
-					  AND 'associations'.'from_type' = '#{@@from_type}'
-					  AND 'associations'.'to_type' = '#{@@to_type}'
-					)
-			  "
-      end
+			if @@class_name
+	      has_many @@to_type, :class_name => @@class_name, :finder_sql => ->(record) do
+	        finder record
+	      end
+			else
+	      has_many @@to_type, :finder_sql => ->(record) do
+	        finder record
+	      end
+			end
     end
 	end
 
   def add_associated obj
   	Associator::Association.create(:from => id, :from_type => @@from_type, :to => obj.id, :to_type => @@to_type, :key => "#{id}_#{obj.id}")
+  	#Association.create(:from => id, :from_type => @@from_type, :to => obj.id, :to_type => @@to_type, :key => "#{id}_#{obj.id}")
 	end
 
+	private
+	def finder record
+		record = self if(record.nil?)
+	    "
+		SELECT '#{@@table_name}'.* FROM '#{@@table_name}'
+			INNER JOIN 'associations' ON ('#{@@table_name}'.'id' = 'associations'.'to'
+			  AND 'associations'.'from' = #{record.id}
+			  AND 'associations'.'from_type' = '#{@@from_type}'
+			  AND 'associations'.'to_type' = '#{@@to_type}'
+			)
+	  "
+	end
 end
